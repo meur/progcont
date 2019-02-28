adfba38f868b00afae7da7a73ffa9d2700ec5786
meur <trolleveraloneyea@gmail.com>
2019-02-28 22:41:32 +0100

    filter kihagyas ArrayList segitsegevel

--- NagyKoltokUtazasai/src/NagyKoltokUtazasai.java
+++ NagyKoltokUtazasai/src/NagyKoltokUtazasai.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
@@ -13,11 +14,10 @@ public class NagyKoltokUtazasai {
 		
 		(new BufferedReader(new InputStreamReader(System.in))).lines()
 		.forEach(line -> {
-			List<String> splitted = Arrays.asList(line.split(":|;"));
-			String varos = splitted.get(0);
+			ArrayList<String> splitted = new ArrayList<>(Arrays.asList(line.split(":|;")));
+			String varos = splitted.remove(0);
 			
 			List<String> szemelyek = splitted.stream()
-					.filter(szo -> !szo.equals(varos))
 					.map(jelenes -> jelenes.split(",")[0])
 					.collect(Collectors.toList());			
 			szemelyek.forEach(szemely -> {
