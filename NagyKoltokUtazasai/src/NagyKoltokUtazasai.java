fbb5055d527fc6f082c14cd310cda033f301d693
meur <trolleveraloneyea@gmail.com>
2019-02-28 20:56:40 +0100

    csoportositas javitas

--- NagyKoltokUtazasai/src/NagyKoltokUtazasai.java
+++ NagyKoltokUtazasai/src/NagyKoltokUtazasai.java
@@ -1,24 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class NagyKoltokUtazasai {
 
 	public static void main(String[] args) {
-		HashMap<String, TreeSet<String>> jelenesek = new HashMap<>();
+		TreeMap<String, TreeSet<String>> jelenesek = new TreeMap<>();
 		
 		(new BufferedReader(new InputStreamReader(System.in))).lines()
 		.forEach(line -> {
-			ArrayList<String> splitted = new ArrayList<>();
-			splitted.addAll(Arrays.asList(line.split(":|;")));
-			jelenesek.put(splitted.remove(0), new TreeSet<>(splitted.stream()
-					.map(darab -> darab.split(",")[0])
-					.collect(Collectors.toList())));
-			;
+			List<String> splitted = Arrays.asList(line.split(":|;"));
+			String varos = splitted.get(0);
+			
+			List<String> szemelyek = splitted.subList(1, splitted.size() - 1).stream()
+					.map(jelenes -> jelenes.split(",")[0])
+					.collect(Collectors.toList());			
+			szemelyek.forEach(szemely -> {
+				if (!jelenesek.containsKey(szemely)) {
+					jelenesek.put(szemely, new TreeSet<>());
+				}
+				jelenesek.get(szemely).add(varos);
+			});
 		});
 		
 		jelenesek.forEach((key, value) -> {
